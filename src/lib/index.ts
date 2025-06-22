7ca0d13a6765aae0917e2fb272f735446762f6dc
FredViken <fredrik.mjos@hotmail.com>
2025-06-22 17:43:18 +0200

    export componet + common ag grid types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // Reexport your entry components here
+export { default as AgGrid } from './AgGrid.svelte';
+
+// Re-export commonly used AG Grid exports for convenience
+export type { GridOptions, Module, GridParams } from 'ag-grid-community';
+export { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
